1034b4918ae6e6345c935ef558f11c31eb86e539
paritosh22777 <paritosh22777@gmail.com>
2023-03-04 18:31:50 +0530

    Initial Commit

---
+++ frontend/build/precache-manifest.e1d0f81d3f2656448ca6ddce52461fb7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2af0fe233b9399a0183e",
+    "url": "/static/css/main.75789f1f.chunk.css"
+  },
+  {
+    "revision": "2af0fe233b9399a0183e",
+    "url": "/static/js/main.2af0fe23.chunk.js"
+  },
+  {
+    "revision": "3ba2fcded166817acffb",
+    "url": "/static/js/1.3ba2fcde.chunk.js"
+  },
+  {
+    "revision": "6350fc8b0fff70880cab",
+    "url": "/static/js/2.6350fc8b.chunk.js"
+  },
+  {
+    "revision": "56441d8c1bf395a8b595",
+    "url": "/static/js/runtime~main.56441d8c.js"
+  },
+  {
+    "revision": "40bc718381a7e47d9c4dfd2c10ad431c",
+    "url": "/static/media/profile-icon.40bc7183.jpeg"
+  },
+  {
+    "revision": "930c2abb6bed575dfe75d36d410a6715",
+    "url": "/static/media/logo-light-black.930c2abb.jpg"
+  },
+  {
+    "revision": "88881f4b4f6353a9c191a5ec7d4ef4f8",
+    "url": "/static/media/logo-white.88881f4b.png"
+  },
+  {
+    "revision": "2d2cbdec024971c459bc0c4f20e59b34",
+    "url": "/static/media/bgimg.2d2cbdec.jpg"
+  },
+  {
+    "revision": "7b8e6115ed055dcd539904f9416dc9ce",
+    "url": "/index.html"
+  }
+];
